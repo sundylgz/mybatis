904d461f03e343cb4b565ab5c51024523565fa2e
sundylgz <adminlgz123>
2015-03-14 10:03:31 +0800

    初始化代码

--- demo02/src/main/java/com/sundy/mybatis/App.java
+++ demo02/src/main/java/com/sundy/mybatis/App.java
@@ -2,12 +2,9 @@ package com.sundy.mybatis;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
